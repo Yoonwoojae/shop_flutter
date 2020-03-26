3d7dfdcc7a8e210f792db71e58e4badfb0015d8a
yoonwoojae <dbsdnwp1004@naver.com>
2020-03-26 11:04:38 +0900

    로직변경

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:shop_flutter/screens/product_detail_screen.dart';
+
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
 
-  ProductItem({
-    this.id,
-    this.title,
-    this.imageUrl,
-  });
+  ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
@@ -18,32 +15,34 @@ class ProductItem extends StatelessWidget {
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
-          onTap: () => Navigator.of(context).pushNamed(
-            ProductDetailScreen.routeName,
-            arguments: id,
-          ),
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: id,
+            );
+          },
           child: Image.network(
             imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
-          backgroundColor: Colors.black54,
+          backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(
-              Icons.favorite,
-              color: Theme.of(context).accentColor,
-            ),
-          ),
-          trailing: IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Theme.of(context).accentColor,
-            ),
+            icon: Icon(Icons.favorite),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
           ),
           title: Text(
             title,
             textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {},
+            color: Theme.of(context).accentColor,
           ),
         ),
       ),
